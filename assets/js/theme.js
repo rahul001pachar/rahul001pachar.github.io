b66898ba715456419e331f270627fdff39d1a355
Rahul Pachar <rahulpachar0786@gmail.com>
2023-07-25 23:21:51 +0530

    update avatar dark

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,7 +8,7 @@ const toDark = (event) => {
     document.querySelector("meta[name='theme-color']").content = "#2a2e35";
     // document.querySelector("#theme").classList.remove("fa-sun");
     document.querySelector("#theme").src = "/images/moon.svg";
-    document.querySelector("#avatar").src = "/images/avatar_dark.PNG";
+    document.querySelector("#avatar").src = "/images/avatar_dark.png";
 
     // document.querySelector("#theme").classList.add("fa-moon");
     document.documentElement.setAttribute("data-theme", "dark");
